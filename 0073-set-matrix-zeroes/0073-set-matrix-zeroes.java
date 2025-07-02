128f2e5128dfd510515010cb01fd2e5d868ff05f
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-07-02 16:05:52 +0530

    Time: 0 ms (100%), Space: 45.7 MB (63.62%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,30 +3,35 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        //to marking 0s
-        boolean []row = new boolean[m];
-        boolean []col = new boolean[n];
+        boolean Row0 = false;
+        boolean Col0 = false;
+    //check any 0s available in 0th row or col
+        for(int i = 0; i < n; i++) if(matrix[0][i] == 0)  Row0 = true;
+        for(int i = 0; i < m; i++) if(matrix[i][0] == 0) Col0 = true;
 
-        //check 0s available or not
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j< n; j++) 
-            {
-                if(matrix[i][j] == 0) {
-                     row[i] = true;
-                     col[j] = true;
-                }    
+    //set the remaining
+    for(int i = 1; i < m; i++){
+        for(int j = 1; j < n ; j++){
+            if(matrix[i][j] == 0){
+                matrix[0][j] = 0;
+                matrix[i][0] = 0;
             }
         }
-        
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j< n; j++){
-                if(row[i]|| col[j]){
-                    matrix[i][j] = 0;
-                }
+    }
+    // then set 0 for 1th row an 1th col to last row and last col
+    for(int i = 1; i < m; i++){
+        for(int j = 1; j < n ; j++){
+            if(matrix[0][j] ==0 || matrix[i][0]== 0){
+                matrix[i][j] = 0;
             }
         }
-
+    }
+    //set 0s for 0th row and col
+    if( Row0)for(int i = 0; i < n; i++){
+        matrix[0][i]= 0;
+    }
+    if(Col0)for(int i = 0; i < m; i++){
+        matrix[i][0] = 0;
+    }
     }
 }
