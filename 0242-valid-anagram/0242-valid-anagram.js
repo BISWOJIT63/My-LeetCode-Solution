22d4b21ffebac81cdc30a1f843c12d2c43cbc7ff
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-07-19 13:35:30 +0530

    Time: 32 ms (16.11%), Space: 57.8 MB (58.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,10 +4,9 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+    if(s.length != t.length) return false;
     const sortedString1 = s.split('').sort().join('');
     const sortedString2 = t.split('').sort().join('');
-    if(sortedString1 === sortedString2){
-        return true;
-    }
-    return false;
+    
+    return sortedString1 === sortedString2;
 };
