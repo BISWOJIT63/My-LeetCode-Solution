56db8a551576dbb4bccfddb61102a6809abcd5cc
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-07-19 14:00:01 +0530

    Time: 5 ms (95.77%), Space: 53.7 MB (99.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,8 +5,13 @@
  */
 var isAnagram = function(s, t) {
     if(s.length != t.length) return false;
-    const sortedString1 = s.split('').sort().join('');
-    const sortedString2 = t.split('').sort().join('');
-    
-    return sortedString1 === sortedString2;
+    const count = new Array(26).fill(0);
+    for(let i = 0; i < s.length; i++){
+        count[s.charCodeAt(i)-97]++;
+        count[t.charCodeAt(i)-97]--;
+    }
+   for(let i = 0; i < 26; i++){
+    if(count[i] !== 0) return false;
+   }
+   return true;
 };
