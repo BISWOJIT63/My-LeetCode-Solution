41634096f434654c04809fe46941720434b78a06
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-07-30 20:05:33 +0530

    Time: 2 ms (86.91%), Space: 45 MB (37.97%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,53 +1,49 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        String[][] board= new String[n][n]; 
+        char[][] board= new char[n][n]; 
         for(int i = 0; i <n ; i++){
-           Arrays.fill(board[i], ".");
+           Arrays.fill(board[i],'.');
         }
         nQueen(0,board,n);
         return ans;
     }
         List<List<String>> ans = new ArrayList<>();
-        private boolean isSafe(int row, int col,String[][] board,int n){
+        private boolean isSafe(int row, int col,char[][] board,int n){
             // upperside 
             for(int i = row-1; i >= 0; i--){
-                if(board[i][col].equals("Q")) return false;
+                if(board[i][col]=='Q') return false;
             }
             //upper-left diagonal
             for(int i = row-1, j = col-1; i >= 0 && j >= 0; i--,j--){
-                if(board[i][j].equals("Q")) return false;
+                if(board[i][j]=='Q') return false;
             }
 
             //upper-right diagonal
              for(int i = row-1, j = col+1; i >= 0 && j < n; i--,j++){
-                if(board[i][j].equals("Q")) return false;
+                if(board[i][j]=='Q') return false;
             }
             return true;
         }
 
-        private void nQueen(int row,String [][] board,int n){
+        private void nQueen(int row,char [][] board,int n){
             if(n == row){
                ans.add(arrayToList(board,n));
                return;
      }
             for(int i = 0; i < n; i++){
                 if(isSafe(row,i,board,n)){
-                    board[row][i] = "Q";
+                    board[row][i] = 'Q';
                     nQueen(row+1,board,n);
-                    board[row][i] = ".";
+                    board[row][i] = '.';
                 }
             }
         }
-        private List<String> arrayToList(String [][]board,int n)
+        private List<String> arrayToList(char [][]board,int n)
         {
             List<String> temp = new ArrayList<>();
                for (int i = 0; i < n; i++) {
-               StringBuilder sb = new StringBuilder();
-               for (int j = 0; j < n; j++) {
-               sb.append(board[i][j]);
+               temp.add(new String(board[i]));
             }
-         temp.add(sb.toString());
-        }
         return temp;
     }
 }
