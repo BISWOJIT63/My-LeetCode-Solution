760080794751cb33390bb9eba1545a1a60f60343
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-07-30 19:38:09 +0530

    Time: 6 ms (19.11%), Space: 45 MB (55.8%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        String[][] board= new String[n][n]; 
+        for(int i = 0; i <n ; i++){
+           Arrays.fill(board[i], ".");
+        }
+        nQueen(0,board,n);
+        return ans;
+    }
+        List<List<String>> ans = new ArrayList<>();
+        private boolean isSafe(int row, int col,String[][] board,int n){
+            // upperside 
+            for(int i = row-1; i >= 0; i--){
+                if(board[i][col].equals("Q")) return false;
+            }
+            //upper-left diagonal
+            for(int i = row-1, j = col-1; i >= 0 && j >= 0; i--,j--){
+                if(board[i][j].equals("Q")) return false;
+            }
+
+            //upper-right diagonal
+             for(int i = row-1, j = col+1; i >= 0 && j < n; i--,j++){
+                if(board[i][j].equals("Q")) return false;
+            }
+            return true;
+        }
+
+        private void nQueen(int row,String [][] board,int n){
+            if(n == row){
+               List<String> temp = new ArrayList<>();
+               for (int i = 0; i < n; i++) {
+               StringBuilder sb = new StringBuilder();
+               for (int j = 0; j < n; j++) {
+               sb.append(board[i][j]);
+            }
+         temp.add(sb.toString());
+        }
+        ans.add(temp);
+     }
+            for(int i = 0; i < n; i++){
+                if(isSafe(row,i,board,n)){
+                    board[row][i] = "Q";
+                    nQueen(row+1,board,n);
+                    board[row][i] = ".";
+                }
+            }
+        }
+}
