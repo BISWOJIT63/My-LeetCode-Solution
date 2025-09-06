2530ddb3929054df5888c00acd21dedafd514510
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-09-06 14:35:23 +0530

    Time: 0 ms (100%), Space: 45.6 MB (68.41%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,36 +2,40 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-
-        boolean Row0 = false;
-        boolean Col0 = false;
-    //check any 0s available in 0th row or col
-        for(int i = 0; i < n; i++) if(matrix[0][i] == 0)  Row0 = true;
-        for(int i = 0; i < m; i++) if(matrix[i][0] == 0) Col0 = true;
-
-    //set the remaining
-    for(int i = 1; i < m; i++){
-        for(int j = 1; j < n ; j++){
-            if(matrix[i][j] == 0){
-                matrix[0][j] = 0;
-                matrix[i][0] = 0;
+        int row[] = new int[m];
+        int col[] = new int[n];
+        for(int i = 0; i < m; i++){
+            row[i] = -1;
+        }
+       for(int i = 0; i < n; i++){
+        col[i] = -1;
+       }
+        for(int i = 0; i< m; i++){
+            for(int j = 0; j < n; j++ ){
+                if(matrix[i][j] == 0){
+                    row[i] = 0;
+                    col[j] = 0;
+                }
             }
         }
-    }
-    // then set 0 for 1th row an 1th col to last row and last col
-    for(int i = 1; i < m; i++){
-        for(int j = 1; j < n ; j++){
-            if(matrix[0][j] ==0 || matrix[i][0]== 0){
-                matrix[i][j] = 0;
+        for(int i = 0; i < m; i++){
+            if(row[i] == 0){
+                int j = 0;
+                while(j < n){
+                    matrix[i][j] = 0;
+                    j++;
+                }
             }
         }
-    }
-    //set 0s for 0th row and col
-    if( Row0)for(int i = 0; i < n; i++){
-        matrix[0][i]= 0;
-    }
-    if(Col0)for(int i = 0; i < m; i++){
-        matrix[i][0] = 0;
-    }
+        for(int j = 0; j < n; j++){
+            if(col[j] == 0){
+                int i = 0;
+                while(i < m){
+                    matrix[i][j] = 0;
+                    i++;
+                }
+            }
+        }
+
     }
 }
