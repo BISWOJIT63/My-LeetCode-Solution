fef7ed9228ceaa44e1544a111a1f2b0234569db6
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-09-06 15:41:14 +0530

    Time: 1 ms (71.39%), Space: 45.7 MB (47.29%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,40 +2,54 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int row[] = new int[m];
-        int col[] = new int[n];
-        for(int i = 0; i < m; i++){
-            row[i] = -1;
-        }
-       for(int i = 0; i < n; i++){
-        col[i] = -1;
-       }
-        for(int i = 0; i< m; i++){
-            for(int j = 0; j < n; j++ ){
-                if(matrix[i][j] == 0){
-                    row[i] = 0;
-                    col[j] = 0;
-                }
-            }
-        }
-        for(int i = 0; i < m; i++){
-            if(row[i] == 0){
-                int j = 0;
-                while(j < n){
-                    matrix[i][j] = 0;
-                    j++;
-                }
-            }
-        }
-        for(int j = 0; j < n; j++){
-            if(col[j] == 0){
-                int i = 0;
-                while(i < m){
-                    matrix[i][j] = 0;
-                    i++;
-                }
-            }
-        }
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
 
+        //check zero in 1st row 
+        for(int i = 0; i < n; i++){
+            if(matrix[0][i] == 0){
+             firstRowZero = true;
+             break;
+            }
+        }
+        //check zero in 1st col
+       for(int i = 0; i < m; i++){
+        if(matrix[i][0] == 0){
+           firstColZero = true;
+           break;
+        }
+       }
+       //marking zero
+       //set 0 in ith rows 1st row if any of zero occure in ith row 
+       //set 0 in ith col's 1st col if any of zero occure in ith col
+        for(int i = 1; i< m; i++){
+            for(int j = 1; j < n; j++ ){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        //set zero for answer according to the mark
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        //set 0 according conditions
+
+        if(firstRowZero){
+             for(int j = 0; j < n; j++){
+                matrix[0][j] = 0;
+             }
+        }
+         if(firstColZero){
+             for(int j = 0; j < m; j++){
+                matrix[j][0] = 0;
+             }
+        }
     }
 }
