39a0f2a5cc4826092ffebeeced39e8b511123e5b
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-10-17 22:21:24 +0530

    Time: 42 ms (23.7%), Space: 52.6 MB (17.01%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        if(nums.length < 3){
-            return new ArrayList<>();
-        }
         Arrays.sort(nums);
-
-        List<List<Integer>> finalAns = new ArrayList<>();
-
-        for(int i = 0; i < nums.length; i++){
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int j = i+1;
-            int k = nums.length-1;
-            int curElement = -(nums[i]); 
-            while(j < k){
-                if(nums[j]+nums[k] == curElement){
-                   finalAns.add(new ArrayList<>(List.of(nums[i],nums[j],nums[k])));
-                    j++;
-                    k--;
-
-                     while (j < k && nums[j] == nums[j - 1]) j++;
-
-                    // Skip duplicates for k
-                    while (j < k && nums[k] == nums[k + 1]) k--;
-                    
-                }else if(nums[j]+nums[k] > curElement){
-                    k--;
-                }else{
-                    j++;
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i < nums.length-1 ; i++ ){
+            if(i != 0 && nums[i] == nums[i-1]) continue;
+            int k = i+1;
+            int j = nums.length-1;
+            
+            while(k < j){
+            List<Integer> curAns = new ArrayList<>();
+            int sum = nums[i]+nums[j]+nums[k];
+            if(sum > 0){
+                  j--;
+            }else if(sum < 0){
+                k++;
+            }else{
+                curAns.add(nums[i]);
+                curAns.add(nums[k]);
+                curAns.add(nums[j]);
+                j--;
+                k++;
+                while(k < j && nums[k] == nums[k-1] ){
+                    k++;
                 }
-            }
+                while(k < j && nums[j] == nums[j+1]){
+                    j--;
+                }   
+                ans.add(curAns);   
+            } 
         }
-        return finalAns;
+     }   
+    return ans;
     }
 }
