f748a555a0ab0c61bcc7181bded65bfd9e071ceb
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-10-17 15:50:01 +0530

    Time: 2 ms (98.68%), Space: 58.5 MB (8.09%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -4,12 +4,13 @@ class Solution {
         int i = 0;
         int j = height.length-1;
         while(i < j){
-            int boundry = Math.min(height[i],height[j]);
-            int wl = boundry*(j-i);
+            int curHeight = Math.min(height[i],height[j]);
+            int wl = curHeight*(j-i);
             maxWater = Math.max(wl,maxWater);
-            if(height[i] < height[j]){
+            while(i < j && height[i] <= curHeight){
                 i++;
-            }else{
+            }
+            while(i < j && height[j] <= curHeight){
                 j--;
             }
         }
