7a2ae25c445df1cdc7d6557b302a9ceb426cbb41
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-10-18 15:22:43 +0530

    Time: 15 ms (89.27%), Space: 44 MB (61.88%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -2,34 +2,30 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        for(int p1 = 0; p1 < nums.length - 2; p1++){
+        for(int p1 = 0; p1 < nums.length ; p1++){
             if( p1 > 0 && nums[p1] == nums[p1-1]) continue;
-            for(int p2 = p1+1; p2 < nums.length - 1; p2++){
+            for(int p2 = p1+1; p2 < nums.length ; p2++){
             if( p2 > p1+1 && nums[p2] == nums[p2-1]) continue;
             int p3 = p2+1;
             int p4 = nums.length-1;
             while(p3 < p4 ){
-                long sum = nums[p1]+nums[p2]+nums[p3]+nums[p4];
+                long sum =(long) nums[p1]+nums[p2]+nums[p3]+nums[p4];
                 if(sum < target){
                     p3++;
                 }
                 else if(sum > target){
                     p4--;
                 }else{
-                   List<Integer> l = new ArrayList<>();
-                   l.add(nums[p1]);
-                   l.add(nums[p2]);
-                   l.add(nums[p3]);
-                   l.add(nums[p4]);
-                   ans.add(l);
+                  
+                   ans.add(Arrays.asList(nums[p1],nums[p2],nums[p3],nums[p4]));
                    p3++;
                    p4--;
-                   while(p3 < p4 && nums[p3] == nums[p3-1]){
+                   while(p3 < p4 && nums[p3] == nums[p3-1])
                     p3++;
-                   }
-                   while(p3 < p4 && nums[p4] == nums[p4+1]){
+        
+                   while(p3 < p4 && nums[p4] == nums[p4+1])
                     p4--;
-                   }
+                   
                    
                 }
             }
