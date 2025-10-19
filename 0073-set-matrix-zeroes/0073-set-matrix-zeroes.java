a2e2d49d727e221bd4ab452a86f165012c399304
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-10-19 21:02:39 +0530

    Time: 0 ms (100%), Space: 45.7 MB (48.63%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -7,12 +7,18 @@ class Solution {
 
         //check for 1st row
         for(int i = 0; i < m; i++){
-            if(matrix[i][0] == 0) first_row_zero = true;
+            if(matrix[i][0] == 0) {
+                first_row_zero = true;
+                break;
+            }
         }
 
         //check for 1st col
         for(int i = 0; i < n; i++){
-            if(matrix[0][i] == 0) first_col_zero = true;
+            if(matrix[0][i] == 0){
+                 first_col_zero = true;
+                  break;
+                }
         }
         //check remaining rows and columns by set zero for 1st row and coumn as marker
         for(int i = 1; i < m; i++){
@@ -26,19 +32,11 @@ class Solution {
         //set 0s according to marker
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
-                if(matrix[i][0] == 0){
-                    matrix[i][j] = 0;
-                }
-                if(matrix[0][j] == 0){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
-        } 
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                
-            }
-        } 
+        }  
         if(first_row_zero){
             for(int i = 0; i < m; i++){
                 matrix[i][0] = 0;
