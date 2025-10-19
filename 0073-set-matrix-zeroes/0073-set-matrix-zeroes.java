d65a4be8a8c3fcc9fb88bcc5f2299cf732aaa8f0
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-10-19 21:16:33 +0530

    Time: 1 ms (68.46%), Space: 45.5 MB (85.12%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -6,16 +6,16 @@ class Solution {
         boolean first_col_zero = false;
 
         //check for 1st row
-        for(int i = 0; i < m; i++){
-            if(matrix[i][0] == 0) {
+        for(int i = 0; i < n; i++){
+            if(matrix[0][i] == 0) {
                 first_row_zero = true;
                 break;
             }
         }
 
         //check for 1st col
-        for(int i = 0; i < n; i++){
-            if(matrix[0][i] == 0){
+        for(int i = 0; i < m; i++){
+            if(matrix[i][0] == 0){
                  first_col_zero = true;
                   break;
                 }
@@ -38,13 +38,13 @@ class Solution {
             }
         }  
         if(first_row_zero){
-            for(int i = 0; i < m; i++){
-                matrix[i][0] = 0;
+            for(int i = 0; i < n; i++){
+                matrix[0][i] = 0;
             }
         }      
         if(first_col_zero){
-            for(int i = 0; i < n; i++){
-                matrix[0][i] = 0;
+            for(int i = 0; i < m; i++){
+                matrix[i][0] = 0;
             }
         } 
     }
