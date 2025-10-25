d6db812ec93693b34fc71449c05976683cdd41e9
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-10-25 12:27:50 +0530

    Time: 43 ms (65.45%), Space: 55.4 MB (30.79%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,49 +1,63 @@
 class Solution {
-    
     public int reversePairs(int[] nums) {
-        if(nums.length == 1) return 0;
-        return mergeSort(nums,0,nums.length-1);
-    }
-    private int mergeSort(int[] nums, int start, int end){
-        
-        if(start >= end){
+        if(nums.length <= 1){
             return 0;
         }
-        int mid = start+(end-start)/2;
-        int count = mergeSort(nums,start,mid) + mergeSort(nums,mid+1,end);
-        int j = mid+1;
-        for(int i = start; i <= mid; i++){
-            while(j <= end && nums[i] > 2L * nums[j] ){
-                j++;
-            }
-            count += j-(mid+1);
-        }
-        merge(nums,start,mid,end);
-        return count;
-
+        return mergeSort(nums,0,nums.length-1);
     }
-    private void merge(int[] nums, int start, int mid, int end){
-        int i = start, j = mid+1;
-        int [] temp = new int[end-start+1];
-        int k=0;
-        while(i <= mid && j <= end){
-            if(nums[i] >= nums[j]){
-               temp[k++] = nums[j];
+    public static int mergeSort(int[] arr, int si, int ei){
+        if(si >= ei){
+            return 0;
+        }
+        int count = 0;
+        int mid = si+(ei-si)/2;
+        count += mergeSort(arr,si,mid);
+        count += mergeSort(arr,mid+1,ei);
+        count += merge(arr,si,mid,ei);
+        return count;
+    }
+    public static int merge(int[] arr,int si, int mid,int ei){
+        int r = mid+1;
+        int count = 0;
+        for(int i = si; i <= mid; i++){
+            while(r <= ei && (long)arr[i] > 2L*arr[r]){
+                r++;
+            }
+            count += r-(mid+1);
+        }
+        int n1 = mid - si +1;
+        int n2 = ei - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for(int i = 0; i < n1; i++){
+            L[i] =arr[si+i];
+        }
+        for(int i = 0; i < n2; i++){
+            R[i] = arr[mid+1+i];
+        }
+        int i = 0;
+        int j = 0;
+        int k = si;
+        while(i < n1 && j < n2){
+            if(L[i] > R[j]){
+                arr[k] = R[j]; 
                 j++;
             }else{
-              temp[k++] = nums[i];
-              i++;
+                arr[k] = L[i];
+                i++;
             }
-
+            k++;
         }
-        while(i <= mid){
-            temp[k++] = nums[i];
+        while(i < n1){
+            arr[k] = L[i];
+            k++;
             i++;
         }
-        while(j <= end ){
-             temp[k++] = nums[j];
-             j++;
+        while(j < n2){
+            arr[k] = R[j];
+            j++;
+            k++;
         }
-        System.arraycopy(temp, 0, nums, start, temp.length);
+        return count;
     }
 }
