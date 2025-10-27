40a596e700c3366f723365cc29bde6ee920f2b92
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-10-27 09:15:00 +0530

    Time: 4 ms (64.32%), Space: 44.4 MB (51.44%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -2,28 +2,12 @@ class Solution {
     public String removeOccurrences(String s, String part) {
         int l1 = s.length();
         int l2 = part.length();
-        Stack<Character> st = new Stack<>();
-        for(int i = 0; i < l1;i++){
-            st.push(s.charAt(i));
-            if(st.size() >= l2){
-                boolean match = true;
-                for(int k = 0; k < l2; k++ ){
-                    if(st.get(st.size()-l2+k) != part.charAt(k)){
-                        match = false;
-                        break;
-                    }
-                }
-                if(match){
-                    for(int k = 0; k < l2; k++){
-                        st.pop();
-                    }
-                }
-            }
-            
-        }
         StringBuilder sb = new StringBuilder();
-        for(char c : st){
+        for(char c : s.toCharArray()){
             sb.append(c);
+            if(sb.length() >= l2 && sb.substring(sb.length()-l2).equals(part)){
+                sb.delete(sb.length()-l2,sb.length());
+            }
         }
         return sb.toString();
     }
