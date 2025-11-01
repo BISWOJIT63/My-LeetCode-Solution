3211b7c141b387c349a24a6bdad35b4dc68e540e
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-11-01 14:30:28 +0530

    Time: 1 ms (99.76%), Space: 55.7 MB (8.84%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,31 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            if(mp.containsKey(nums[i])){
-                mp.replace(nums[i],mp.get(nums[i])+1);
-            }else{
-                mp.put(nums[i],1);
+    public static int count(int[] nums,int n, int lo,int hi){
+        int count = 0;
+        for(int i = lo; i <= hi; i++){
+            if(nums[i] == n){
+                count++;
             }
         }
-            for(int key : mp.keySet()){
-                if(mp.get(key) > nums.length/2){
-                    return key;
-                }
-            }
-        return -1;
+        return count;
+    }
+    public static int majorityElementRec(int[] nums, int lo, int hi){
+        if(hi == lo){
+            return nums[lo];
+        }
+        int mid = lo+(hi-lo)/2;
+        int left = majorityElementRec(nums,lo,mid);
+        int right = majorityElementRec(nums,mid+1,hi);
+        if(left == right){
+            return left;
+        }
+        int leftCount = count(nums,left,lo,hi);
+        int rightCount = count(nums,right,lo,hi);
+        return (leftCount > rightCount) ? left :right;
+    }
+    public int majorityElement(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
+        return majorityElementRec(nums,0,nums.length-1);
     }
 }
