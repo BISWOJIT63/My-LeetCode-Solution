a8df3efe794b0c922f2039f27fd21ad66f6c3e46
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-12-01 20:59:13 +0530

    Time: 2113 ms (5.25%), Space: 88.4 MB (5.23%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,57 +1,50 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         //find nextSmallerRight
-        int[] nsr = nextSmallerRight(heights);
+        int[] nsr = new int[heights.length];
+        Stack<Integer> s = new Stack<>();
+        for(int i = heights.length-1; i >=0 ; i--){
+            while(!s.isEmpty() && heights[i] <= heights[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty() ){
+                nsr[i] = heights.length;
+            }else{
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }        
         //find nextSmallerLeft
-        int[] nsl = nextSmallerLeft(heights);
-        int[] area = new int[heights.length];
+        int[] nsl = new int[heights.length];
+        s = new Stack<>();
+        for(int i = 0; i < heights.length ; i++){
+            while(!s.isEmpty() && heights[i] <= heights[s.peek()]){
+                
+                s.pop() ;
+            }
+            if(s.isEmpty()){
+                nsl[i] =-1 ;
+            }else{
+                nsl[i] =s.peek() ;
+            }
+            s.push(i);
+        }
         print(nsr);
         print(nsl);
+        int[] area = new int[heights.length];
+        int maxArea = 0;
         for(int i = 0; i < heights.length; i++){
             int h = heights[i];
             int w = nsr[i]-nsl[i]-1;
             area[i] = h*w;
-        }
-        int maxArea = 0;
-        for(int n : area){
-            maxArea = Math.max(n,maxArea);
+            maxArea = Math.max(area[i],maxArea);
         }
         return maxArea;
-
     }
-    public static void print(int[] nsr){
-        for(int n : nsr){
+    public static void print(int[] arr){
+        for(int n: arr){
             System.out.print(n+" ");
         }
-         System.out.println();
-    }
-    public int[]  nextSmallerRight(int[] heights){
-        int[] nsr = new int[heights.length];
-        Stack<Integer> s = new Stack<>();
-        for(int i = 0; i < heights.length; i++){
-            while(!s.isEmpty() && heights[i] < heights[s.peek()]){
-                nsr[s.pop()] = i;
-            }
-            s.push(i);
-        }
-        while(!s.isEmpty() ){
-                nsr[s.pop()] = heights.length;
-            }
-        return nsr;
-    }
-    public int[]  nextSmallerLeft(int[] heights){
-        int[] nsl = new int[heights.length];
-        Stack<Integer> s = new Stack<>();
-        for(int i = heights.length-1; i >= 0 ; i--){
-            while(!s.isEmpty() && heights[i] < heights[s.peek()]){
-                
-                nsl[s.pop()] = i;
-            }
-            s.push(i);
-        }
-        while(!s.isEmpty() ){
-                nsl[s.pop()] = -1;
-            }
-        return nsl;
+        System.out.println();        
     }
 }
