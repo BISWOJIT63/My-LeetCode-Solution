554642788f589fb6b828a1a6d3e78e1d903b43b5
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2025-12-14 21:13:55 +0530

    Time: 12 ms (8.74%), Space: 43.8 MB (5.87%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -2,15 +2,8 @@ class Solution {
     public int twoCitySchedCost(int[][] costs) {
         int n = costs.length;
         //sort the array basis of profit the 1st half for 
-        Arrays.sort(costs,(a,b)->{
-            
-            int profitA = a[1]-a[0];
-            int profitB = b[1]-b[0];
+        Arrays.sort(costs, (a, b) -> (a[0] - a[1]) - (b[0] - b[1]));
 
-            if(profitA > profitB ) return -1;
-            else if(profitA < profitB ) return +1;
-            else return 0;
-        });
         int maxProfit = 0;
         for(int i = 0; i < n/2; i++){
             maxProfit += costs[i][0];
