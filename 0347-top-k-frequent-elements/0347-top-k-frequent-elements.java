e9b09fd244f02fcdf6c76947fb42585f7caf6ebd
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2026-02-16 23:00:57 +0530

    Time: 15 ms (51.99%), Space: 48.1 MB (14.79%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,28 +2,15 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         int[] ans = new int[k];
         HashMap<Integer, Integer> hm = new HashMap<>();
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
         for (int i = 0; i < nums.length; i++) {
             hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
         }
-        
-        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-            maxHeap.add(entry.getValue());
-        }
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->hm.get(b)-hm.get(a));
+        maxHeap.addAll(hm.keySet());
         int i = 0;
         while (i < k) {
-            ans[i++] = getKeyFromValue(hm, maxHeap.poll());
+            ans[i++] = maxHeap.poll();
         }
         return ans;
-    }
-
-    public int getKeyFromValue(HashMap<Integer, Integer> hm, int value) {
-        for (int key : hm.keySet()) {
-            if (hm.get(key) == value) {
-                hm.remove(key);
-                return key;
-            }
-        }
-        return -1; 
     }
 }
