7de537d1207ee97ae383b3908084654b06c7aeab
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2026-03-02 09:29:49 +0530

    Time: 0 ms (100%), Space: 43.9 MB (66.13%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,35 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int si = 0;
-        int ei = nums.length-1;
-        while(si <= ei){
-            int mid = si+(ei-si)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            if(nums[mid] >= nums[si]){
-                if(nums[mid] >= target && target >= nums[si] ){
-                    ei = mid-1;
-                }else{
-                    si = mid+1;
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low <= high){
+            if(nums[low] == target) return low;
+            if(nums[high] == target) return high;
+            int mid = low+(high-low)/2;
+            if(nums[mid] == target) return mid; // check the target is mid 
+
+            //if left half is sorted
+            if(nums[low] <= nums[mid]){
+                //check taget exist in the left part
+                if(nums[low] <= target && target < nums[mid]){
+                    high = mid-1;
+                }
+                //if not exist check in right side
+                else{
+                    low = mid+1;
                 }
             }
-             else{
-                if(nums[mid] <= target && target <= nums[ei]){
-                    si = mid+1;
-                }else{
-                    ei = mid-1;
+            //if right half sorted
+            else{
+                //check target exist in right half
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid+1;
+                }else{ // check in left half
+                    high = mid-1;
                 }
             }
         }
-        return -1;
+        return -1; // if target is not exist 
     }
 }
