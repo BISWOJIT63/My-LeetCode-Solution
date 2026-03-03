43214a190a30dd1d3333843596adff6cc8dfecd3
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2026-03-03 20:55:45 +0530

    Time: 2 ms (95.32%), Space: 45.8 MB (51.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -13,6 +13,7 @@ class Solution {
             return;
         }
         for(int i = cur; i < arr.length;i++){
+            if(t < arr[i]);
             l.add(arr[i]);
             backtracking(arr,t-arr[i],ans,l,i);
             l.remove(l.size()-1);
