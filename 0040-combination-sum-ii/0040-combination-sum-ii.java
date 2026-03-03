ebb18406f9e214a6b498360d6f1431aeac218469
Biswojit Sahoo <sahoobiswojit165@gmail.com>
2026-03-03 21:25:50 +0530

    Time: 5 ms (99.23%), Space: 44.9 MB (93.73%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,32 +1,29 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] arr, int t) {
         List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
         List<Integer> l = new ArrayList<>();
-        Arrays.sort(candidates);
-
-        findAns(candidates,target,ans,l,0);
+        backtracking(arr, t, ans, l, 0);
         return ans;
     }
-    public static void findAns(int[] arr, int tar,List<List<Integer>> ans ,List<Integer> l,int start){
-        if(tar == 0){
-            if(!isExist(ans,l)){
-                ans.add(new ArrayList<>(l));
 
-            }
+    public void backtracking(int[] arr, int t, List<List<Integer>> ans, List<Integer> l, int cur) {
+        if (t == 0) {
+               ans.add(new ArrayList<>(l));
             return;
         }
-        if(tar < 0){
+        if (t < 0) {
             return;
         }
-        for(int i = start; i < arr.length; i++){
-            if(i > start && arr[i] == arr[i-1]) continue;
-            if(tar < arr[i]) break;
-            l.add(arr[i]);
-            findAns(arr,tar-arr[i],ans,l,i+1);
-            l.remove(l.size()-1);
+        for (int i = cur; i < arr.length; i++) {
+            if(i > cur && arr[i] == arr[i-1]) continue;
+            if(arr[i] > t) break;
+            l.add(arr[i]);            
+            backtracking(arr, t - arr[i], ans, l, i+1);
+            l.remove(l.size() - 1);
         }
     }
-    public static boolean isExist(List<List<Integer>> ans ,List<Integer> l){
-        return (ans.contains(l));
+    public boolean isExist(List<List<Integer>> ans, List<Integer> l){
+        return ans.contains(l);
     }
 }
